e165c7b796ea82293f454b9e7550aea4120efe9a
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-07-05 20:05:18 -0300

    map inserted in the form

--- src/Pages/CreatePoint/index.tsx
+++ src/Pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './styles.css'
 import logo from '../../assets/logo.svg'
@@ -59,6 +60,15 @@ const CreatPoint = () => {
             <h2>Endereco</h2>
             <span>Selecione o endereco no mapa</span>
           </legend>
+
+          <Map center={[-22.1080366,-43.1697286]} zoom={15} >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-22.1080366,-43.1697286]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
