6bcdc684a2886a324d1440485c63688dacb939c3
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-07-07 20:15:49 -0300

    Loaded items from api

--- src/Pages/CreatePoint/index.tsx
+++ src/Pages/CreatePoint/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 
+import api from '../../services/api'
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatPoint = () => {
+  const [ items, setItems ] = useState<Item[]>([])
+
+  useEffect (() => {
+    api.get('items').then(response => {
+      setItems(response.data)
+    })
+  }, [])
   return (
     <div id="page-create-point">
       <header>
@@ -93,30 +107,14 @@ const CreatPoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
+            {items.map(item => (
+              <li key={item.id} >
+                <img src={item.image_url} alt={item.title}/>
+                <span>{item.title}</span>
             </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-              <span>Oleo de cozinha</span>
-            </li>
+            ))}
+            
+            
           </ul>
         </fieldset>
         
